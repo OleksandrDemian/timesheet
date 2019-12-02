919d2b405d05945ab7d0d3b28843d435457d30ad
Luca Crema <lc.crema@hotmail.com>
2019-12-02 20:05:20 +0100

    renamed methods and clearer specifications

--- client/src/utils/Days.js
+++ client/src/utils/Days.js
@@ -11,12 +11,13 @@ const DAYS = [
 ];
 
 /**
- * I have no idea what this method does
+ * Parses days on week numbering from (sun-sat / 0 - 6) to (mon - sun / 1 - 7)
+ * 
  * @param {number} day
- * @returns 7 if day is 0, day otherwise
+ * @returns the number of the day in the week (1 to 7)
  * @throws Error if day > 6
  */
-function toDay(day) {
+function parseSunSatToMonSunWeekNumbering(day) {
     if(day === 0){
         day = 7;
     } else if(day > 6){
@@ -27,21 +28,28 @@ function toDay(day) {
 }
 
 /**
- * What the fuck does this method do? Souldn't it return 1 all the time?
- * @param {number} month 
- * @param {number} year 
+ * @param {number} month number (1 to 12) of the month
+ * @param {number} year needed if it's a year when february has 29 days
+ * @returns number of days for the month
  */
-export function getDaysCount(month, year) {
+export function getMonthNumberOfDays(month, year) {
     return new Date(year, month+1, 0).getDate();
 }
 
+/**
+ * Retrieves every day of the month information and puts it in an ordered array
+ * 
+ * @param {number} month desired month
+ * @param {number} year desired year
+ * @return an ordered array of Day for the month
+ */
 export function getDays(month, year) {
-    let days = getDaysCount(month, year);
+    let days = getMonthNumberOfDays(month, year);
     let ret = [];
 
     for(let d = 1; d <= days; d++){
         let date = new Date(year, month, d);
-        let day = toDay(date.getDay());
+        let day = parseSunSatToMonSunWeekNumbering(date.getDay());
         let appDay = DAYS[day-1];
 
         ret.push(new Day({
